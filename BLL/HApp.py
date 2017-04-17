6978ab507a0108d46596b959cf9e072efddaa73e
FelixMailwriter <emailwriter7@GMAIL.COM>
2017-04-17 17:23:04 +0300

    2

--- BLL/HApp.py
+++ BLL/HApp.py
@@ -2,9 +2,9 @@
 
 import os
 from PyQt4 import uic
-from PyQt4.Qt import QObject
-from PyQt4 import QtCore
-from Objects import ObjHandler
+from PyQt4.Qt import QObject, QMessageBox
+from PyQt4 import QtCore, QtGui
+from BLL.ItemsController import ItemsController
 from Magazines import MagazinesController
 from Report import ReportController
 
@@ -14,20 +14,10 @@ class HApp(QObject):
     def __init__(self):
         QObject.__init__(self)
         self.form=MainWindow()
-        self.ObjHandler=ObjHandler(self.form)
-        self.MagazinesController=None
-        self.ReportController=None
-        self.form.window.tabWidget.currentChanged.connect(self.tabSwitchHandler)
+        self.ItemsController=ItemsController(self.form)
+        self.MagazinesController=MagazinesController(self.form)
+        self.ReportController=ReportController(self.form)
         self.form.window.show()
-        
-        
-    def tabSwitchHandler(self, tabNum):
-        if tabNum==0:
-            self.ObjHandler=ObjHandler(self.form)
-        elif tabNum==1:
-            self.MagazinesController=MagazinesController(self.form)
-        elif tabNum==2:
-            self.ReportController=ReportController(self.form)
         
 class MainWindow(QObject):
 
@@ -35,9 +25,29 @@ class MainWindow(QObject):
         QObject.__init__(self)
         path=os.path.abspath("UI/UIForms/mainForm.ui")
         self.window=uic.loadUi(path)
-        self.magController=None
-  
-    def refreshMagazinesTable(self):
-        pass              
-                  
+        self.ItemTable=self.window.ItemTable
+
+        
+        
+    def fillItemsTable(self, rows):
+        self.ItemTable.setRowCount(0)
+        counter=0
+        for row in rows:
+            Item0=QtGui.QTableWidgetItem(str(row[0]))
+            Item1=QtGui.QTableWidgetItem(row[1])
+            Item2=QtGui.QTableWidgetItem(str(row[2]/100.0))
+
+            self.ItemTable.insertRow(counter)
+            self.ItemTable.setItem(counter,0,Item0)
+            self.ItemTable.setItem(counter,1,Item1)
+            self.ItemTable.setItem(counter,2,Item2)
+            counter+=1             
+    
+    def getCurrentItems(self):
+        return self.ItemTable.selectedItems()
+                   
+    def setIcon(self, qpixmap):
+        self.window.ibl_ItemIcon.setPixmap(qpixmap)    
+
+                          
                   
